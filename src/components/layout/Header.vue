443f8e661dbc669964b5c55aa1f3028527c0cb29
fengyong <fengyong@xdstar.net>
2019-03-14 11:26:05 +0800

    layout布局实现

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -30,7 +30,7 @@
                         {{username}} <i class="el-icon-caret-bottom"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
-                        <a href="http://blog.gdfengshuo.com/about/" target="_blank">
+                        <a href="" target="_blank">
                             <el-dropdown-item>关于作者</el-dropdown-item>
                         </a>
                         <a href="https://github.com/fdy696/vue-admin-template" target="_blank">
